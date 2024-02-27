ad8e479c5a79d487beece0070eba4e8c8cf2f8aa
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-02-27 22:15:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let currAudio;
+document.addEventListener('click',(e)=>{
+	if(e.target.classList.contains('btn')){
+		let audioFile = e.target.getAttribute('data-audio');
+		const audio = new Audio(audioFile); 
+		if (currAudio) {
+	      currAudio.pause();
+	    }
+	    currAudio = audio;
+		audio.play();
+	}
+	else if(e.target.classList.contains('stop')){
+		if (currAudio) {
+	      currAudio.pause();
+	    }
+	}
+})
